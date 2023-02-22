d18e7cb516dbd3d3f3f65b4c22b050fd8b5a1eb3
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-02-22 20:43:35 +0400

    second commit

---
+++ rust_2/target/debug/deps/rust_2.d
@@ -0,0 +1,5 @@
+C:\Users\sul_s\Documents\rust_practice\rust_2\target\debug\deps\rust_2.exe: src\main.rs
+
+C:\Users\sul_s\Documents\rust_practice\rust_2\target\debug\deps\rust_2.d: src\main.rs
+
+src\main.rs:
---
+++ rust_2/target/release/rust_2.d
@@ -0,0 +1,1 @@
+C:\Users\sul_s\Documents\rust_practice\rust_2\target\release\rust_2.exe: C:\Users\sul_s\Documents\rust_practice\rust_2\src\main.rs
