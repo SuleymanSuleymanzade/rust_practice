429d563cc87a41e2fa438e37e27fd49bdf70dc5c
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-02-22 21:34:44 +0400

    string 2

--- rust_2/src/main.rs
+++ rust_2/src/main.rs
@@ -1,4 +1,17 @@
+use std;
+
 fn main() {
-    let a: i32 = 5;
+    let mut a: i32 = 5;
+    let _c: i32 = 54;
+    let _x: i32 = 54;
+    let y: i32 = 54 * 432;
+    
     println!("Hello, world! {}", a);
+    a += 1;
+    println!("hello world {}", a);
+    println!("y= {}", y);
+    println!("The max val of int {}", std::i32::MAX);
+    let _num = 43;
+    println!("Several values {:?}",(32,43,2,42))
+
 }
