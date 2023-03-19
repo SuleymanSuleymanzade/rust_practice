4367933c54ceff78383796f488e1a06a772cc261
Suleyman Suleymanzade <sul_soft@hotmail.com>
2023-03-19 17:04:40 +0400

    smart pointers

--- box_smartpointer/src/main.rs
+++ box_smartpointer/src/main.rs
@@ -1,14 +1,10 @@
+
 #[derive(Debug)]
 enum List{
     Cons(i32, Option<Box<List>>),
     Nil,
 }
 use List::{Cons, Nil};
-
-#[derive(Debug)]
-enum UpdatedList{
-
-}
 
 struct MySmartpointer{
     value:i32,
@@ -20,6 +16,21 @@ impl MySmartpointer{
     }
 }
 
+use std::ops::Deref;
+
+impl Deref for MySmartpointer{
+    type Target = i32;
+    fn deref(&self) -> &i32{
+        &self.value
+    }
+}
+
+impl Drop for MySmartpointer{
+    fn drop(&mut self){
+        println!("dropping my smartpointer from mem {:?}", self.value);
+    }
+}
+
 fn main() {
     let list:List = List::Cons(23, Some(Box::new(Cons(11, Some(Box::new(Cons(55, None)))))));
     println!("{:?}", list);
@@ -28,6 +39,10 @@ fn main() {
     let b = &a;
     println!("{}", 50 == a);
     println!("{}", 50 == *b);
-    println!("{}", a == *b);
+    //println!("{}", a == b);
+    let sptr1 = MySmartpointer::new(*b);
+    let sptr2 = MySmartpointer::new(*b);
+    println!("{}", a==*sptr1);
 
+    drop(sptr1);
 } 
